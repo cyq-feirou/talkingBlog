41da66c3847eebdbd3dc00106407c326764addb3
chenyuqing <3037964849@qq.com>
2020-01-14 10:33:39 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React,  { useState, useEffect } from 'react';
-import  { BrowserRouter as Router, Route } from "react-router-dom";
+import React,  { useEffect } from 'react';
+import  { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
 import HomePage from "./pages/home/home.js";
-import LoginPage from "./pages/login/login.js";
-import RegisterPage from "./pages/login/register.js";
-import RegisterSuccessPage from "./pages/success/registerSuccess.js";
-
+import './App.scss';
 function AppPage() {
+   
     //当传空数组[]时，就是当组件将被销毁时才进行解绑，这也就实现了componentWillUnmount的生命周期函数
     useEffect(()=>{
         //异步，每次加载或者更新都会执行
         console.log(new Date().getTime());
     },[])
     return (
-        <div>
-            <Router>
-                <Route exact path="/loginPage" component={LoginPage}/>
-                <Route exact path="/registerPage" component={RegisterPage}/>
-                <Route exact path="/registerSuccess" component={RegisterSuccessPage}/>
-                <Route path="/homePage/index" exact component={HomePage}/>
-            </Router>
+        <div className="banApp">
+            <div className="banApp-wrapper">
+                <Router>
+                    <Route path="/home/:viewsId" exact component={HomePage}/>
+                </Router>
+            </div>
         </div>
     )
 }
